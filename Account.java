0cefc74dab7639822a32206aee9606fc8c4497ae
pranavniar <54687317+pranavniar@users.noreply.github.com>
2022-04-23 16:05:14 +0530

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,68 @@
+package com.example.BookManagmentApplication.model;
+
+public class Account {
+
+	 private int userId;
+	 private String userName;
+	 private String password;
+	 private String phoneNumber;
+	 private String email;
+	 private String address;
+	 private String userType;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getUserType() {
+		return userType;
+	}
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+	 
+	private void resetPassword() {
+		
+	}
+	
+	private void login() {
+		
+	}
+	
+	private void changeName() {
+		
+	}
+	
+	
+}
---
+++ Admin.java
@@ -0,0 +1,34 @@
+package com.example.BookManagmentApplication.model;
+
+import org.springframework.web.servlet.ModelAndView;
+
+public class Admin extends Account{
+
+	
+	
+	public ModelAndView deleteUser() {
+		
+	}
+	
+	public ModelAndView deleteBook() {
+		
+	}
+			
+	public ModelAndView viewUserPublishedBooks() {
+		
+	}
+	
+	public ModelAndView viewUserBorrowedBooks() {
+		
+	}
+	
+	public ModelAndView viewUserTransactionHistory() {
+		
+	}
+	
+	public ModelAndView viewUserPendingDues() {
+		
+	}
+
+	
+}

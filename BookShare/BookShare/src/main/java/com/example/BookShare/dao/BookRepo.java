d0140b1a1889864b4dd2614e3fc0317a9e31c677
Nirav Parmar <54586517+Nirav0702@users.noreply.github.com>
2022-04-26 01:04:26 +0530

    Update BookRepo.java

--- BookShare/BookShare/src/main/java/com/example/BookShare/dao/BookRepo.java
+++ BookShare/BookShare/src/main/java/com/example/BookShare/dao/BookRepo.java
@@ -6,12 +6,19 @@ import com.example.BookShare.model.Account;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.BookShare.model.Book;
+import com.example.BookShare.model.Wallet;
 
 public interface BookRepo extends CrudRepository<Book, Integer>
 {
+	List<Book> findAllByBookTitle(String bookTitle);
 
-}
+	List<Book> findAllByOwnerName(String ownerName);
+
+	List<Book> findAllByBorrowerName(String borrowerName);
+}	

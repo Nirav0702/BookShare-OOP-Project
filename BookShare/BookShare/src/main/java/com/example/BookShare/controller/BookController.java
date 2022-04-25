54f5e22b64abfb4ce4ffee650953dd46874b6cc2
Nirav Parmar <54586517+Nirav0702@users.noreply.github.com>
2022-04-25 12:28:24 +0530

    Update BookController.java

--- BookShare/BookShare/src/main/java/com/example/BookShare/controller/BookController.java
+++ BookShare/BookShare/src/main/java/com/example/BookShare/controller/BookController.java
@@ -3,6 +3,8 @@ package com.example.BookShare.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,40 +13,29 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.BookShare.dao.BookRepo;
 import com.example.BookShare.model.Book;
-import com.example.BookShare.services.BookService;
+//import com.example.BookShare.services.BookService;
 
 @Controller
 public class BookController {
 	@Autowired
-	private BookService bookService;
 	BookRepo bookrepo;
 
 	@RequestMapping("/addBook")
-	public String addBook(@RequestParam("bookTitle")String bookTitle, @RequestParam("bookAuthor")String bookAuthor, @RequestParam("bookPublisher")String bookPublisher, @RequestParam("bookYear")String bookYear, @RequestParam("bookEdition")String bookEdition) 
+	public String addBook(Book book, @RequestParam("userName")String userName) 
 	{
-		Book book = new Book();
-		book.setBookTitle(bookTitle);
-		book.setBookAuthor(bookAuthor);
-		book.setBookPublisher(bookPublisher);
-		book.setBookYear(bookYear);
-		book.setBookEdition(bookEdition);
-		//book.setBookId(1);
 		book.setAvailability(true);
-		book.setOwnerId(1);
-		book.setBorrowerId(2);
+		book.setOwnerName(userName);
+		book.setBorrowerName("N/A");
 		book.setTimeBorrowed("01012022");
-		book.setTimereturned("01022022");
+		book.setTimereturned("N/A");
+		System.out.println(userName);
 		bookrepo.save(book);
 		return "dashboard.jsp";
 	}
 	
 	@GetMapping("/yourBooks")
-	public String getBooks(Model model) {
-		
-		List <Book> booklist = bookService.getBooks();
-		
-		model.addAttribute("books", booklist);
-		
+	public String getBooks(Model model) 
+	{
 		return "yourbooks.jsp";
 	}
 }
